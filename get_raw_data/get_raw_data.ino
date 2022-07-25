c2fa9df2874a1cd6359dc1bbadf60132a9b52283
FernandoMoralesM01 <fernandomoralesm01@gmail.com>
2022-07-25 14:10:23 -0500

    Código para la placa con esp32.
    Se realizó un código para conectar los acelerómetros a la placa esp32.
    El código contiene funciones para leer, acceder a los datos crudos y mandar los datos de aceleración y velocidad angular al puerto serial.
    En una futura actualización la conexión será inalámbrica
    .

--- get_raw_data/get_raw_data.ino
+++ get_raw_data/get_raw_data.ino
@@ -3,54 +3,87 @@
 #include "Wire.h"
 #include "stdio.h"
 
-MPU6050 segmento_1;
-MPU6050 segmento_2;
-MPU6050 segemento_3;
+#define NUM_MPU 3
+
+MPU6050 MPU_1(0x68);
+MPU6050 MPU_2(0x69);
 
 int ax, ay, az;
 int gx, gy, gz;
+int select = 0;
 
-float aceleracion[3][3];
-float velocidad_ang[3][3];
+float aceleracion[NUM_MPU][3];
+float velocidad_ang[NUM_MPU][3];
 
-void setup() 
+void setup()
 {
   Serial.begin(9600);         //Iniciando puerto serial
-  Wire.begin();               //Iniciando I2C  
-  segmento_1.initialize();    //Iniciando el sensor
-  if (segmento_1.testConnection()) Serial.println("Sensor iniciado correctamente");
-  else Serial.println("Error al iniciar el sensor");
-}
+  Wire.begin();               //Iniciando I2C
+  MPU_1.initialize();    //Iniciando el sensor
+  if (MPU_1.testConnection()) Serial.println("s");
+  else Serial.println("no");
+  //delay(2000);
+  MPU_2.initialize();    //Iniciando el sensor
+  if (MPU_2.testConnection()) Serial.println("s");
+  else Serial.println("no");
+  //delay(2000);
+ 
+}   
 
 void Obtener_acelarcion (int n);
 void Obtener_velocidadAng (int n);
+void Manda_Serial(int n);
 
-void loop() 
+void loop()
 {
-    String str_aceleracion;
-    String str_velocidadAng;
-    segmento_1.getAcceleration(&ax, &ay, &az);
-    segmento_1.getRotation(&gx, &gy, &gz);
+  MPU_1.getAcceleration(&ax, &ay, &az);
+  MPU_1.getRotation(&gx, &gy, &gz);
 
-    Obtener_acelarcion (0);
-    Obtener_velocidadAng (0);
+  Obtener_acelarcion (0);
+  Obtener_velocidadAng (0);
+  Manda_Serial(0);
+
+  //delay(100);
   
-    str_aceleracion = "A: " + String (aceleracion[0][0], 3) + ", " + String (aceleracion[0][1], 3)+ ", " + String (aceleracion[0][2], 3);
-    str_velocidadAng = "W: " + String (velocidad_ang[0][0], 3) + ", " + String (velocidad_ang[0][1], 3)+ ", " + String (velocidad_ang[0][2], 3);
-    Serial.println(str_aceleracion);
-    Serial.println(str_velocidadAng);
+  MPU_2.getAcceleration(&ax, &ay, &az);
+  MPU_2.getRotation(&gx, &gy, &gz);
+
+  Obtener_acelarcion (1);
+  Obtener_velocidadAng (1);
+  Manda_Serial(1);
+ 
 }
+
 
 void Obtener_acelarcion (int n)
 {
-  aceleracion [n][0] = ax * (9.81/16384.0);
-  aceleracion [n][1] = ay * (9.81/16384.0);
-  aceleracion [n][2] = az * (9.81/16384.0);
+  aceleracion [n][0] = ax * (9.81 / 16384.0);
+  aceleracion [n][1] = ay * (9.81 / 16384.0);
+  aceleracion [n][2] = az * (9.81 / 16384.0);
 }
 
 void Obtener_velocidadAng (int n)
 {
-  velocidad_ang [n][0] = gx * (250.0/32768.0);
-  velocidad_ang [n][1] = gy * (250.0/32768.0);
-  velocidad_ang [n][2] = gz * (250.0/32768.0);
+  velocidad_ang [n][0] = gx * (250.0 / 32768.0);
+  velocidad_ang [n][1] = gy * (250.0 / 32768.0);
+  velocidad_ang [n][2] = gz * (250.0 / 32768.0);
+}
+
+void Manda_Serial(int n)
+{
+  String str_aceleracion;
+  String str_velocidadAng;
+  if(n == 0)
+  {
+    str_aceleracion = "A: " + String (aceleracion[n][2], 3) + "," + String (aceleracion[n][1], 3) + "," + String (aceleracion[n][0], 3);
+    str_velocidadAng = "W: " + String (velocidad_ang[n][2], 3) + "," + String (velocidad_ang[n][1], 3) + "," + String (velocidad_ang[n][0], 3);
+  }
+  else
+  {
+    str_aceleracion = "p: " + String (aceleracion[n][2], 3) + "," + String (aceleracion[n][1], 3) + "," + String (aceleracion[n][0], 3);
+    str_velocidadAng = "l: " + String (velocidad_ang[n][2], 3) + "," + String (velocidad_ang[n][1], 3) + "," + String (velocidad_ang[n][0], 3);
+  }
+  
+  Serial.println(str_aceleracion);
+  Serial.println(str_velocidadAng);
 }
---
+++ get_raw_data_ESP32/get_raw_data_ESP32.ino
@@ -0,0 +1,99 @@
+#include <Adafruit_MPU6050.h>
+
+#define NUM_MPU 3
+
+int mpuSelect = 0; 
+int pinSelect[NUM_MPU] = {15, 2, 4};
+
+
+Adafruit_MPU6050 mpu;
+
+Adafruit_Sensor *mpu_temp, *mpu_accel, *mpu_gyro;
+
+sensors_event_t accel;
+sensors_event_t gyro;
+
+void setup() 
+{
+  Serial.begin(9600);
+  //while (!Serial)
+  //  delay(10); // will pause Zero, Leonardo, etc until serial console opens
+  if (!mpu.begin()) {
+      Serial.println("Failed");
+      while (1) {
+        delay(10);
+      }
+    }
+  Serial.println("Conectado");
+  mpu_accel = mpu.getAccelerometerSensor();
+  mpu_gyro = mpu.getGyroSensor();
+  
+  for(int i = 0; i < NUM_MPU; i++)
+    pinMode(pinSelect[i], OUTPUT);
+  
+  
+}
+
+void Seleccionar_mpu();
+void Obtener_aceleracion(int N_mpu);
+void Obtener_velocidadAng(int N_mpu);
+void Manda_serial(int N_mpu);
+
+void loop() 
+{
+  Seleccionar_mpu();
+  Obtener_aceleracion();
+  Obtener_velocidadAng();
+  Manda_serial();
+  mpuSelect++;
+  if(mpuSelect > NUM_MPU)
+      mpuSelect = 0;
+}
+
+void Seleccionar_mpu()
+{
+  for (int i = 0; i < NUM_MPU; i++)
+    if(i == mpuSelect)
+      digitalWrite(pinSelect[i], LOW);
+    else
+      digitalWrite(pinSelect[i], HIGH);
+  delay(10);
+  
+}
+
+void Obtener_aceleracion()
+{
+  mpu_accel->getEvent(&accel);
+}
+
+void Obtener_velocidadAng()
+{
+  mpu_gyro->getEvent(&gyro);
+}
+
+void Manda_serial()
+{
+  String str_aceleracion;
+  String str_velocidadAng;
+
+  switch (mpuSelect)
+  {
+    case 0:
+      str_aceleracion = "A: " + String (accel.acceleration.x, 3) + "," + String (accel.acceleration.y, 3) + "," + String (accel.acceleration.z, 3);
+      str_velocidadAng = "W: " + String (gyro.gyro.x, 3) + "," + String (gyro.gyro.y, 3) + "," + String (gyro.gyro.y, 3);
+      Serial.println(str_aceleracion);
+      Serial.println(str_velocidadAng);
+    break;
+    case 1:
+      str_aceleracion = "S: " + String (accel.acceleration.x, 3) + "," + String (accel.acceleration.y, 3) + "," + String (accel.acceleration.z, 3);
+      str_velocidadAng = "E: " + String (gyro.gyro.x, 3) + "," + String (gyro.gyro.y, 3) + "," + String (gyro.gyro.y, 3);
+      Serial.println(str_aceleracion);
+      Serial.println(str_velocidadAng);
+    break;
+    default:
+    break;
+  }
+  
+  
+  //delay(10);
+}
